b3e91b667dc4812047d8c7e1571aa0b9c623a110
franco-canedo <francanedo1@hotmail.com>
2020-04-25 20:07:48 -0500

    loghts css

--- src/GameComponents/RightComponentGame.js
+++ src/GameComponents/RightComponentGame.js
@@ -7,19 +7,40 @@ class RightComponentProfile extends Component {
             <div className="RightComponentGame">
                 <div className="lightsContainer">
                     <div className="lights">
-                        <div className="spanLights"></div>
+
+                        <div className="spanLights">
+                            <div className="speed">
+                                <h4>Speed:</h4>
+                            </div>
+                        </div>
+
+
                     </div>
                     <div className="lights">
-                        <div className="spanLights"></div>
+                        <div className="spanLights">
+                            <div className="speed">
+                                <h4>Speed:</h4>
+                            </div>
+                        </div>
                     </div>
                     <div className="lights">
-                        <div className="spanLights"></div>
+                        <div className="spanLights">
+                            <div className="speed">
+                                <h4>Speed:</h4>
+                            </div>
+                        </div>
                     </div>
                     <div className="lights">
-                        <div className="spanLights"></div>
+                        <div className="spanLights">
+                            <div className="speed">
+                                <h4>Speed:</h4>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="rightCompBottomDiv"></div>
+                <div className="rightCompBottomDiv">
+                    <h2>Lead: Dane</h2>
+                </div>
 
             </div>
         );
